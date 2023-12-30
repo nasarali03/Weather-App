4fb678e1f4307d835950e98b7f48fb96e0535f2b
nasarali03 <nasarali1805@gmail.com>
2023-12-30 12:05:23 +0500

    Add error message

--- script.js
+++ script.js
@@ -36,33 +36,40 @@ form.addEventListener("submit", async (event) => {
     console.log("Error:", error);
   }
 
-  cityName.innerText = city;
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    document.querySelector(".error").style.display = "none";
+    cityName.innerText = city;
 
-  //getting current time
-  const currentDate = new Date();
-  const hours = currentDate.getHours();
-  const minutes = currentDate.getMinutes();
-  const currentTime = `${hours}:${minutes}`;
-  time.innerText = currentTime;
+    //getting current time
+    const currentDate = new Date();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const currentTime = `${hours}:${minutes}`;
+    time.innerText = currentTime;
 
-  //calculating temperature in degree Celsius
-  let temp = response.main.temp;
-  temp = Number(temp);
-  temp = temp - 273.15;
-  temp = Math.round(temp);
-  temperature.innerText = `${temp}°C`;
+    //calculating temperature in degree Celsius
+    let tempvar = response.main.temp;
+    tempvar = Number(tempvar);
+    tempvar = tempvar - 273.15;
+    tempvar = Math.round(tempvar);
+    temperature.innerText = `${tempvar}°C`;
 
-  presentCondition.innerText = response.weather[0].main;
+    presentCondition.innerText = response.weather[0].main;
 
-  let windSpeedinMps = Number(response.wind.speed);
-  let windSpeedinKph = windSpeedinMps * 3.6;
-  windSpeedinKph = Math.floor(windSpeedinKph);
-  windSpeed.innerText = `${windSpeedinKph}Km/h`;
-  windIcon.style.display = "inline";
+    let windSpeedinMps = Number(response.wind.speed);
+    let windSpeedinKph = windSpeedinMps * 3.6;
+    windSpeedinKph = Math.floor(windSpeedinKph);
+    windSpeed.innerText = `${windSpeedinKph}Km/h`;
+    windIcon.style.display = "inline";
 
-  humidity.innerText = `${response.main.humidity}%`;
-  humidityIcon.style.display = "inline";
+    humidity.innerText = `${response.main.humidity}%`;
+    humidityIcon.style.display = "inline";
 
-  const icon = response.weather[0].icon;
-  imageIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png" width="100px"> `;
+    const icon = response.weather[0].icon;
+    imageIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png" width="100px"> `;
+    document.querySelector(".weather").style.display = "block";
+  }
 });
